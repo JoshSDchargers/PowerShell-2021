d86c2aa2d3e3d3a23868c46adcad57eeec4f5cf9
Joshua Aguirre <30274013+JoshSDchargers@users.noreply.github.com>
2021-11-18 11:24:47 -0800

    Update RemoveDisableAccountsOneGroup.ps1

--- RemoveDisableAccountsOneGroup.ps1
+++ RemoveDisableAccountsOneGroup.ps1
@@ -1,4 +1,4 @@
-﻿Get-ADUser -SearchBase ‘OU=Staff,OU=Users,OU=astc,DC=astc,DC=local’ -Filter * 
+﻿Get-ADUser -SearchBase ‘OU=#’ -Filter * 
 
 $Group = Staff
 $Users = Get-ADGroupMember $Group | % {Get-ADUser $_.SamAccountName} | Where Enabled -EQ $false

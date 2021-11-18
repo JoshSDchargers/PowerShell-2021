27319f1b4c84bdd7425a650dbb2af9f27a807241
Joshua Aguirre <30274013+JoshSDchargers@users.noreply.github.com>
2021-11-18 11:23:59 -0800

    Add files via upload

---
+++ RemoveDisableAccountsOneGroup.ps1
@@ -0,0 +1,5 @@
+﻿Get-ADUser -SearchBase ‘OU=Staff,OU=Users,OU=astc,DC=astc,DC=local’ -Filter * 
+
+$Group = Staff
+$Users = Get-ADGroupMember $Group | % {Get-ADUser $_.SamAccountName} | Where Enabled -EQ $false
+Remove-ADGroupMember $Group -Members $Users
---
+++ newUser.ps1
@@ -0,0 +1,8 @@
+﻿$NewUser = Read-Host -Prompt 'Enter New Users Username'
+$Department = Read-Host -Prompt 'Enter New Users Department'
+New-ADUser -Name "$NewUser" -UserPrincipalName "$newUser@astci.com" -Path "OU=Staff,OU=Users,OU=astc,DC=astc,DC=local" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
+<# New-ADUser -Name "$NewUser" -UserPrincipalName "$newUser@astci.com" -Path "OU=$Department,OU=Staff,OU=Users,OU=astc,DC=astc,DC=local" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true #>
+
+
+
+

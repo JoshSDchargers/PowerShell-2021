30f005c72ad0483f6d925dbcbe4d3f43cc50875f
Joshua Aguirre <30274013+JoshSDchargers@users.noreply.github.com>
2021-11-18 11:24:30 -0800

    Update newUser.ps1

--- newUser.ps1
+++ newUser.ps1
@@ -1,7 +1,7 @@
 ﻿$NewUser = Read-Host -Prompt 'Enter New Users Username'
 $Department = Read-Host -Prompt 'Enter New Users Department'
-New-ADUser -Name "$NewUser" -UserPrincipalName "$newUser@astci.com" -Path "OU=Staff,OU=Users,OU=astc,DC=astc,DC=local" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
-<# New-ADUser -Name "$NewUser" -UserPrincipalName "$newUser@astci.com" -Path "OU=$Department,OU=Staff,OU=Users,OU=astc,DC=astc,DC=local" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true #>
+New-ADUser -Name "$NewUser" -UserPrincipalName "$newUser@astci.com" -Path "OU=#" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true
+<# New-ADUser -Name "$NewUser" -UserPrincipalName "$newUser@astci.com" -Path "OU=#" -AccountPassword(Read-Host -AsSecureString "Input Password") -Enabled $true #>
 
 
 
